7d2c652d73b5e10d1a0c4b06ab510807a67f3a0a
airemsezer <azimeiremsezer@gmail.com>
2025-07-21 23:41:38 +0300

    ...

---
+++ task-2.js
@@ -0,0 +1,18 @@
+function makeArray(firstArray , secondArray , maxLength){
+    let newArray = firstArray.concat(secondArray);
+    if(newArray.length > maxLength){
+        let fixedSizeArray=[];
+        for(let i= 0; i<maxLength; i++){
+            fixedSizeArray[i] = newArray[i];
+        }
+        return fixedSizeArray;
+    }
+    return newArray;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
